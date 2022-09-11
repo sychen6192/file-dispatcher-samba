f5662c915eb2c3078a8d47d87588ec14d6ce5c85
sychen6192 <qazxsw9295@gmail.com>
2022-09-11 18:35:50 +0800

    expand to news

--- t1.py
+++ t1.py
@@ -6,9 +6,33 @@ from smb.SMBConnection import SMBConnection
 from arp import scan_port, scan
 from concurrent.futures import ThreadPoolExecutor
 import socket
-
+from tkinter import N,S,W,E
 
 def main():
+	def openf():
+		global filename
+		filetypes = (
+			('text files', '*.txt'),
+			('All files', '*.*')
+		)
+		filename = fd.askopenfilename(
+			title='Open a file',
+			initialdir='/',
+			filetypes=filetypes)
+		if filename:
+			showinfo(
+				title='Selected File',
+				message=filename
+			)
+
+	def scan_hosts():
+		tree.delete(*tree.get_children())
+		results = scan("192.168.1.1/24")
+		with ThreadPoolExecutor(max_workers=6) as executor:
+			hosts = executor.map(scan_port, results)
+		for host in hosts:
+			if host:
+				tree.insert('', tk.END, values=host)
 	root = tk.Tk()
 	w = 950 # width for the Tk root
 	h = 550 # height for the Tk root
@@ -32,33 +56,6 @@ def main():
 	filemenu.add_cascade(label='File', menu=mb1)
 	filemenu.add_cascade(label='Scan', menu=mb2)
 	filemenu.add_cascade(label='Help', menu=mb3)
-
-	def openf():
-		global filename
-		filetypes = (
-			('text files', '*.txt'),
-			('All files', '*.*')
-		)
-
-		filename = fd.askopenfilename(
-			title='Open a file',
-			initialdir='/',
-			filetypes=filetypes)
-
-		showinfo(
-			title='Selected File',
-			message=filename
-		)
-
-	def scan_hosts():
-		tree.delete(*tree.get_children())
-		results = scan("192.168.1.1/24")
-		with ThreadPoolExecutor(max_workers=6) as executor:
-			hosts = executor.map(scan_port, results)
-		for host in hosts:
-			if host:
-				tree.insert('', tk.END, values=host)
-
 	mb1.add_command(label='Open Files', command=openf)
 	mb2.add_command(label='Rescan host', command=scan_hosts)
 	mb3.add_command(label='Run')
@@ -67,6 +64,9 @@ def main():
 	tree = ttk.Treeview(root, columns=columns, show='headings')
 	tree.heading("host", text="Host")
 	tree.heading("ip", text="IP")
+	tree.grid(sticky=(N, S, W, E))
+	root.grid_rowconfigure(0, weight=1)
+	root.grid_columnconfigure(0, weight=1)
 	scan_hosts()
 
 	def upload_file(remote_name, ip, service, path='/'):
@@ -84,11 +84,12 @@ def main():
 			record = item['values']
 			try:
 				if filename:
-					print(record[0], record[1])
 					upload_file(record[0], record[1], 'MP')
-					showinfo(title='Upload Success', message=':'.join(record))
+					showinfo(title='File Dispatcher', message=f'Upload Success - {record[1]}')
+			except NameError:
+				showinfo('File Dispatcher', 'Please set your upload file.')
 			except Exception as err:
-				showinfo('File Dispatcher', 'File not exist!!')
+				print(err)
 
 	tree.bind('<<TreeviewSelect>>', item_selected)
 
@@ -96,8 +97,6 @@ def main():
 	scrollbar = ttk.Scrollbar(root, orient=tk.VERTICAL, command=tree.yview)
 	tree.configure(yscroll=scrollbar.set)
 	scrollbar.grid(row=0, column=1, sticky='ns')
-	tree.grid(row=0, column=0, sticky="w" + "e" + "n" + "s")
-
 	root.mainloop()
 
 
