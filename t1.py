cfa4cec5f6213288ff60be33942fa18c1c279ac7
Sychen <qazxsw9295@gmail.com>
2022-09-18 18:48:31 +0800

    label

--- t1.py
+++ t1.py
@@ -1,12 +1,15 @@
 import tkinter as tk
-import tkinter.ttk as ttk
+from tkinter import ttk
 from tkinter.messagebox import showinfo
 from tkinter import filedialog as fd
 from smb.SMBConnection import SMBConnection
 from arp import scan_port, scan
 from concurrent.futures import ThreadPoolExecutor
+from PIL import ImageTk, Image
 import socket
 from tkinter import N,S,W,E
+
+filename = ""
 
 def main():
 	def openf():
@@ -15,59 +18,21 @@ def main():
 			('text files', '*.txt'),
 			('All files', '*.*')
 		)
-		filename = fd.askopenfilename(
-			title='Open a file',
-			initialdir='/',
-			filetypes=filetypes)
+		filename = fd.askopenfilename(title='Open a file',initialdir='/',filetypes=filetypes)
+		print(filename)
 		if filename:
-			showinfo(
-				title='Selected File',
-				message=filename
-			)
+			ent.delete("0", tk.END)
+			ent.insert("0", filename)
 
 	def scan_hosts():
 		tree.delete(*tree.get_children())
 		results = scan("192.168.1.1/24")
 		with ThreadPoolExecutor(max_workers=6) as executor:
 			hosts = executor.map(scan_port, results)
+		hosts = [("host1", "192.168.1.1"), ("host2", "192.168.1.2"), ("host3", "192.168.1.3")]
 		for host in hosts:
 			if host:
-				tree.insert('', tk.END, values=host)
-	root = tk.Tk()
-	w = 950 # width for the Tk root
-	h = 550 # height for the Tk root
-
-	ws = root.winfo_screenwidth() # width of the screen
-	hs = root.winfo_screenheight() # height of the screen
-
-	x = (ws/2) - (w/2)
-	y = (hs/2) - (h/2)
-
-	# set the dimensions of the screen
-	# and where it is placed
-	root.geometry('%dx%d+%d+%d' % (w, h, x, y))
-	root.title("File Dispatcher")
-	root.iconbitmap('folder.ico')
-	filemenu = tk.Menu(root, tearoff=False)
-	root.config(menu=filemenu)
-	mb1 = tk.Menu(filemenu)
-	mb2 = tk.Menu(filemenu)
-	mb3 = tk.Menu(filemenu)
-	filemenu.add_cascade(label='File', menu=mb1)
-	filemenu.add_cascade(label='Scan', menu=mb2)
-	filemenu.add_cascade(label='Help', menu=mb3)
-	mb1.add_command(label='Open Files', command=openf)
-	mb2.add_command(label='Rescan host', command=scan_hosts)
-	mb3.add_command(label='Run')
-	mb3.add_command(label='Help')
-	columns = ('host', 'ip')
-	tree = ttk.Treeview(root, columns=columns, show='headings')
-	tree.heading("host", text="Host")
-	tree.heading("ip", text="IP")
-	tree.grid(sticky=(N, S, W, E))
-	root.grid_rowconfigure(0, weight=1)
-	root.grid_columnconfigure(0, weight=1)
-	scan_hosts()
+				tree.insert('', tk.END, values=host, tags="unchecked")
 
 	def upload_file(remote_name, ip, service, path='/'):
 		conn = SMBConnection('username', 'password', 'any_name', remote_name)
@@ -78,25 +43,103 @@ def main():
 			except Exception as err:
 				print(err)
 
-	def item_selected(event):
-		for selected_item in tree.selection():
-			item = tree.item(selected_item)
-			record = item['values']
+	def batch_upload_file(service="MB"):
+		for server in selected_host:
 			try:
-				if filename:
-					upload_file(record[0], record[1], 'MP')
-					showinfo(title='File Dispatcher', message=f'Upload Success - {record[1]}')
-			except NameError:
-				showinfo('File Dispatcher', 'Please set your upload file.')
-			except Exception as err:
-				print(err)
+				print(f'upload {filename} to {server[0]}/{service}')
+				upload_file(server[0], server[1], service)
+			except ConnectionRefusedError:
+				print("chaonima")
 
-	tree.bind('<<TreeviewSelect>>', item_selected)
+	# def item_selected(event):
+	# 	for selected_item in tree.selection():
+	# 		item = tree.item(selected_item)
+	# 		record = item['values']
+	# 		try:
+	# 			if filename:
+	# 				upload_file(record[0], record[1], 'MP')
+	# 				showinfo(title='File Dispatcher', message=f'Upload Success - {record[1]}')
+	# 		except NameError:
+	# 			showinfo('File Dispatcher', 'Please set your upload file.')
+	# 		except Exception as err:
+	# 			print(err)
 
-	# add a scrollbar
-	scrollbar = ttk.Scrollbar(root, orient=tk.VERTICAL, command=tree.yview)
-	tree.configure(yscroll=scrollbar.set)
-	scrollbar.grid(row=0, column=1, sticky='ns')
+	selected_host = []
+	def toggleCheck(event):
+		rowid = tree.identify_row(event.y)
+		tag = tree.item(rowid, "tags")[0]
+		tags = list(tree.item(rowid, "tags"))
+		tags.remove(tag)
+		tree.item(rowid, tags=tags)
+		if tag == "checked":
+			tree.item(rowid, tags="unchecked")
+			selected_host.remove(tree.item(rowid)["values"])
+		else:
+			tree.item(rowid, tags="checked")
+			selected_host.append(tree.item(rowid)["values"])
+		print(selected_host)
+
+
+	root = tk.Tk()
+	w = 950 # width for the Tk root
+	h = 550 # height for the Tk root
+
+	ws = root.winfo_screenwidth() # width of the screen
+	hs = root.winfo_screenheight() # height of the screen
+
+	x = (ws/2) - (w/2)
+	y = (hs/2) - (h/2)
+
+	root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+	root.title("File Dispatcher")
+	root.iconbitmap('folder.ico')
+	filemenu = tk.Menu(root)
+	root.config(menu=filemenu)
+	mb1 = tk.Menu(filemenu, tearoff=0)
+	mb2 = tk.Menu(filemenu, tearoff=0)
+	mb3 = tk.Menu(filemenu, tearoff=0)
+	filemenu.add_cascade(label='檔案', menu=mb1)
+	filemenu.add_cascade(label='掃描', menu=mb2)
+	filemenu.add_cascade(label='幫助', menu=mb3)
+	mb1.add_command(label='選擇檔案', command=openf)
+	mb2.add_command(label='重新掃描主機', command=scan_hosts)
+	mb3.add_command(label='獲得幫助')
+	wrapper1 = tk.LabelFrame(root, text="Host List")
+	wrapper2 = tk.LabelFrame(root, text="File")
+	wrapper1.pack(fill="both", expand="yes", padx=20, pady=10)
+	wrapper2.pack(fill="both", expand="yes", padx=20, pady=10)
+	tree = ttk.Treeview(wrapper1, columns=(1, 2), height=5)
+	style = ttk.Style(tree)
+	style.configure("Treeview", rowheight=30)
+
+	im_checked = ImageTk.PhotoImage(Image.open("check.png").resize((25, 25)))
+	im_unchecked = ImageTk.PhotoImage(Image.open("uncheck.png").resize((25, 25)))
+	tree.tag_configure('checked', image=im_checked)
+	tree.tag_configure('unchecked', image=im_unchecked)
+
+	tree.pack(expand=True, fill='both')
+	tree.heading("#0", text="")
+	tree.heading("#1", text="主機名稱")
+	tree.heading("#2", text="IP")
+
+	tree.bind('<Button 1>', toggleCheck)
+
+	# File section
+	ent = tk.Entry(wrapper2, width=50)
+	ent.pack(side=tk.LEFT, padx=6)
+	btn = tk.Button(wrapper2, text='Load File', command=openf)
+	btn.pack(side=tk.LEFT, padx=6)
+
+	dbtn = tk.Button(wrapper2, text='Dispatch', command=batch_upload_file)
+	dbtn.pack(side=tk.RIGHT, padx=6)
+
+	scan_hosts()
+
+
+
+	# tree.bind('<<TreeviewSelect>>', item_selected)
+
+
 	root.mainloop()
 
 
